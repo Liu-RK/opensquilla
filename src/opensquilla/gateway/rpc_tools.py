staruphackers/opensquilla-tw

Make sandbox access explicit and grant-scoped
This lands sandbox run modes, path/network grant persistence, managed network enforcement, and approval surfaces behind explicit scoped choices.

Constraint: PR #189 targets sandbox-optimization and GitHub CI passed for d94b2fecdbe4d2184a100b25fbfb32b89241dfaf.
Rejected: Treating CLI approvals as legacy once/always prompts | sandbox choices encode grant scope and must stay explicit.
Confidence: high
Scope-risk: broad
Directive: Preserve typed sandbox approval choices across Web, CLI, and TUI resolver paths.
Tested: GitHub CI result passed; Ubuntu lint/test/build passed; Windows lint/test/build passed; local targeted approval and sandbox tests passed; local ruff and mypy passed.
Not-tested: Release packaging contracts were skipped by CI impact classification.

src/opensquilla/gateway/rpc_tools.py
@@ -6,6 +6,8 @@ import os
 from typing import Any
 
 from opensquilla.gateway.rpc import RpcContext, get_dispatcher
+from opensquilla.sandbox.integration import run_in_process_network_action
+from opensquilla.sandbox.types import DenialResult
 from opensquilla.tools.builtin.web import (
     get_active_provider,
     run_web_search_payload,
@@ -220,11 +222,35 @@ async def _handle_search_query(params: dict | None, ctx: RpcContext) -> dict[str
     provider_name = str(provider) if provider else None
     if provider_name:
         search_runtime_status(provider_name)
-    payload = await run_web_search_payload(
-        query,
-        _query_limit(params),
-        provider_name=provider_name,
+    limit = _query_limit(params)
+
+    async def _run_search() -> dict[str, Any]:
+        return await run_web_search_payload(
+            query,
+            limit,
+            provider_name=provider_name,
+        )
+
+    payload_or_denial = await run_in_process_network_action(
+        action_kind="web.fetch",
+        argv=("web_search", query, str(limit or "")),
+        callback=_run_search,
     )
+    if isinstance(payload_or_denial, DenialResult):
+        denial = payload_or_denial
+        return {
+            "ok": False,
+            "query": query,
+            "provider": provider_name or get_active_provider(),
+            "results": [],
+            "error": {
+                "kind": denial.reason.value,
+                "class": "SandboxDenied",
+                "message": denial.message,
+                "retryable": denial.retryable,
+            },
+        }
+    payload = payload_or_denial
     error = payload.get("error")
     if payload.get("ok", False):
         result = {

Liu-RK/opensquilla

src/opensquilla/gateway/rpc_tools.py
@@ -6,6 +6,8 @@ import os
 from typing import Any
 
 from opensquilla.gateway.rpc import RpcContext, get_dispatcher
+from opensquilla.sandbox.integration import run_in_process_network_action
+from opensquilla.sandbox.types import DenialResult
 from opensquilla.tools.builtin.web import (
     get_active_provider,
     run_web_discover_payload,
@@ -220,11 +222,35 @@ async def _handle_search_query(params: dict | None, ctx: RpcContext) -> dict[str
     provider_name = str(provider) if provider else None
     if provider_name:
         search_runtime_status(provider_name)
-    payload = await run_web_discover_payload(
-        query,
-        _query_limit(params),
-        provider_name=provider_name,
+    limit = _query_limit(params)
+
+    async def _run_search() -> dict[str, Any]:
+        return await run_web_discover_payload(
+            query,
+            limit,
+            provider_name=provider_name,
+        )
+
+    payload_or_denial = await run_in_process_network_action(
+        action_kind="web.fetch",
+        argv=("web_search", query, str(limit or "")),
+        callback=_run_search,
     )
+    if isinstance(payload_or_denial, DenialResult):
+        denial = payload_or_denial
+        return {
+            "ok": False,
+            "query": query,
+            "provider": provider_name or get_active_provider(),
+            "results": [],
+            "error": {
+                "kind": denial.reason.value,
+                "class": "SandboxDenied",
+                "message": denial.message,
+                "retryable": denial.retryable,
+            },
+        }
+    payload = payload_or_denial
     error = payload.get("error")
     if payload.get("ok", False):
         result = {